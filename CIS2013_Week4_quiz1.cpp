93e40ab57b817c52f22767e4e23d96c03f529e1d
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-02-07 19:09:16 -0600

    add foo and bar

--- CIS2013_Week4_quiz1.cpp
+++ CIS2013_Week4_quiz1.cpp
@@ -6,13 +6,23 @@ using namespace std;
 
 
 int main() {
-	
+	int FOO = 0;
+	int BAR = 0;
+
 	for (int i = 0; i <= 2000; i++) {
 		cout << i;
-		if (i % 50 == 0) { cout << "FOO"; }
-		if (i % 33 == 0) { cout << "BAR"; }
+		if (i % 50 == 0) { cout << "FOO";
+		FOO++;
+ }
+		if (i % 33 == 0) { cout << "BAR";
+		BAR++;
+ }
 		cout << endl;
 	}
+
+	cout << "Total number of FOO" << FOO++ << endl;
+	cout << "Total number of BAR" << BAR++  << endl;
+	
 	char done;
 	cin >> done;
 	
